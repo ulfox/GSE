ed5f5c0e110f7eefa51bde3cad7a5f96ec967b38
Christos.K <freedomrfox@gmail.com>
2017-07-11 05:50:44 +0300

    Updated fetch, extract, verify and extract functions

---
+++ config.d/controller/modules/functions/cnetwork.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# SIMPLE DHCP NETWORKING
+if _call_net; then
+	_ctflag_net=0
+	export _ctflag_net
+fi
+
+# CHECK NETWORK FLAG AND FETCH VERSION AND CONFIG.D DIRECTORY
+if [[ "${_ctflag_net}" ]]; then
+	# DEFINE BEST ACTIVE SERVER
+	_server_exp
+
+	# SOURCES EXP
+	_sources_exp
+
+	# FETCH CONFIG.D
+	_fetch_confd
+	if [[ "$_ctflag_confd}" == 0 ]]; then
+		# EXPORT NEW CONFIGS
+		_bsu_dfs
+	fi
+
+	# MOUNT SYSFS AS RO
+	_mount_sysfs "/mnt/workdir"
+	# CHECK LOCAL VERSION OF SYSFS WITH SERVERS VERSION
+	_check_version
+fi
