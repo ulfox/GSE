ab8a116483359c47785ea545c21eaeaa58e72564
Christos.K <freedomrfox@gmail.com>
2017-08-22 13:45:35 +0300

    Test script

---
+++ config.d/controller/modules/init_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Creating tmp_dir"
+if mkdir -p tmp_dir; then
+	echo "Created"
+	echo "Mounting /dev/sda5 to tmp_dir"
+	
+	if mount -t ext4 -o rw /dev/sda5 tmp_dir; then
+		echo "Mounted"
+		_proceed_var=0
+	else
+		_proceed_var=1
+	fi
+else
+	_proceed_var=1
+fi
+
+
