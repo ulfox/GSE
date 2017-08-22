d1204ecf17dfb5481a295d647bb3df0d5ad5eb0b
Christos.K <freedomrfox@gmail.com>
2017-08-22 19:43:35 +0300

    Moved bsu to preliminary

--- config.d/controller/modules/functions/ct_newsys.sh
+++ config.d/controller/modules/functions/ct_newsys.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-_bsu_dfs() {
-	#/DEV/SDX
-	SYSDEV="$(blkid | grep "SYSFS" | awk -F ':' '{ print $1 }')"
-	BACKUPDEV="$(blkid | grep "BACKUPFS" | awk -F ':' '{ print $1 }')"
-	BOOTDEV="$(blkid | grep "BOOTFS" | awk -F ':' '{ print $1 }')"
-	USERDATADEV="$(blkid | grep "USERDATAFS" | awk -F ':' '{ print $1 }')"
-
-	export SYSDEV
-	export BACKUPDEV
-	export USERDATADEV
-	export BOOTDEV
-
-	# EXPORT SYSTEM'S FS
-	SYSFS="$(blkid | grep "LABEL=\"SYSFS\"" | awk -F ' ' '{print $4}' | awk -F '=' '{print $2}' | sed 's/\"//g')"
-	export SYS
-
-	# EXPORT BACKUP's FS
-	BACKUPFS="$(blkid | grep "LABEL=\"BACKUPFS\"" | awk -F ' ' '{print $4}' | awk -F '=' '{print $2}' | sed 's/\"//g')"
-	export BACKUPFS
-}
-
 _remake() {
 	if eval "mkfs.$1" "$2" "$3" "$4"; then
 		return 0
